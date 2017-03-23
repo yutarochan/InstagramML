61e498f7ffb39e109ff3c340b4b3003b999a8d6c
quillan86 <bill.dusch@gmail.com>
2017-03-22 21:24:14 -0400

    Data changes
    Added dataframe method to convert dictionary to dataframe for all but
    annotations

--- src/data.py
+++ src/data.py
@@ -3,6 +3,8 @@ InstagramML - Data Parser & Preprocessing
 Author: Yuya Jeremy Ong (yuyajeremyong@gmail.com)
 '''
 import json
+import pandas as pd
+import numpy as np
 
 def parse_json(filename):
     json_file = open(filename, 'rb')
@@ -11,5 +13,70 @@ def parse_json(filename):
 def download_images(target_folder, json):
     parse_json
 
-parse_json('../data/dataset.json')[0]['username']
-parse_json('../data/dataset.json')[0]['id']
+def dataframe(filename):
+    raw_data = parse_json(filename)
+    num_accounts = len(raw_data)
+    columns = ['likes', 'username', 'id', 'date', 'instagram_id', 'thumbnail_src', 'display_src', 'video',
+               'height', 'width', 'caption']
+    # get total number of files
+    size = 0
+    for i in range(num_accounts):
+        size += len(raw_data[i]['posts'])
+    index = [x for x in range(size)]
+    df = pd.DataFrame(index=index, columns=columns)
+    x = 0
+    for i in range(num_accounts):
+        username = str(raw_data[i]['username'])
+        id = int(raw_data[i]['id'])
+        posts = raw_data[0]['posts']
+        for j in range(len(posts)):
+            # Add posts to updated
+            df['likes'][x] = posts[j]['instagram']['likes']['count']
+            df['username'][x] = username
+            df['id'][x] = id
+            df['date'][x] = posts[j]['instagram']['date']
+            df['instagram_id'][x] = posts[j]['instagram']['id']
+            df['thumbnail_src'][x] = posts[j]['instagram']['thumbnail_src']
+            df['display_src'][x] = posts[j]['instagram']['display_src']
+            df['video'][x] = posts[j]['instagram']['is_video']
+            df['height'][x] = posts[j]['instagram']['dimensions']['height']
+            df['width'][x] = posts[j]['instagram']['dimensions']['width']
+            df['caption'][x] = posts[j]['instagram']['caption']
+            annotations = posts[j]['annotations']
+            # ** insert annotation code here... **
+            # faceAnnotations
+            if 'faceAnnotations' in annotations:
+                pass
+            if 'labelAnnotations' in annotations:
+                pass
+            if 'textAnnotations' in annotations:
+                pass
+            if 'webDetection' in annotations:
+                pass
+            if 'imagePropertiesAnnotation' in annotations:
+                pass
+            if 'fullTextAnnotation' in annotations:
+                pass
+            if 'cropHintsAnnotation' in annotations:
+                pass
+            if 'safeSearchAnnotation' in annotations:
+                pass
+            # count for dataframe
+            x += 1
+    return df
+
+
+filename = '../data/dataset.json'
+
+# Test
+raw_data = parse_json(filename)
+# print(raw_data[0]['posts'][0]['instagram']['caption'])
+
+annotations = raw_data[0]['posts'][0]['annotations']
+print('labelAnnotations' in annotations)
+print(raw_data[0]['posts'][432]['annotations'].keys())
+print(annotations['labelAnnotations'])
+
+
+# df = dataframe(filename)
+# print(df.head())
